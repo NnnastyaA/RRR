3c2c63fed7fce52cf8cb7b9032e4bec5a12184d7
NnnastyaA <nastyafedorets@mail.ru>
2026-06-30 15:25:29 +0200

    moved the folder components

--- rrr-project/src/App.jsx
+++ rrr-project/src/App.jsx
@@ -3,9 +3,9 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
-import UserProfile from "./assets/components/UserProfile.jsx";
-import TaskList from "./assets/components/TaskList.jsx";
-import ShoppingBacket from "./assets/components/ShoppingBacket.jsx";
+import UserProfile from "./components/UserProfile.jsx";
+import TaskList from "./components/TaskList.jsx";
+import ShoppingBacket from "./components/ShoppingBacket.jsx";
 
 function App() {
     return (
